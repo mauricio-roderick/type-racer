a561b3c613573a44197efa46f5f963709c521bfd
Erick <roderick.mauricio@headhuntr.io>
2021-03-26 12:49:03 +0800

    Add collection config items

--- src/config/collection.js
+++ src/config/collection.js
@@ -1,14 +1,14 @@
-export const timeZone = ''
 export const dateFormat = {
   client: 'M/D/YY',
   api: 'YYYY-MM-DD'
 }
-
 export const dateTimeFormat = {
   client: 'M/D/YY hh:mm a z',
   api: 'YYYY-MM-DDTHH:mm:ssZ'
 }
-
+export const notifMessage = {
+  internalError: 'Something went wrong.'
+}
 export const global = {
   notifKey: 'GLOBAL_NOTIF'
 }
