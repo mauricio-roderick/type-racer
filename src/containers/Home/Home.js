a46eca67b3c8a9af2b258cfc93f3e680c5c5607a
Erick <roderick.mauricio@headhuntr.io>
2021-03-26 12:48:30 +0800

    Add Home Container

---
+++ src/containers/Home/Home.js
@@ -0,0 +1,14 @@
+import React, { PureComponent } from 'react'
+import Layout from '@containers/Layout/Layout'
+
+class Home extends PureComponent {
+  render () {
+    return (
+      <Layout>
+        <div className="text-center">Temporary Home Page</div>
+      </Layout>
+    )
+  }
+}
+
+export default Home
