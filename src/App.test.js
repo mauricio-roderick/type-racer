24c0f07d915bf9c7eee95f57a10f208d397a8f74
Erick <roderick.mauricio@headhuntr.io>
2021-03-28 19:58:48 +0800

    App component test

--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,13 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 
-// import App from './App'
-// import { storeFactory } from '../test/utils'
+import App from './App'
 
-// const setUp = (initialState) => {
-//   const store = storeFactory(initialState)
-//   const wrapper = shallow(<App store={store} />)
-//   return wrapper
-// }
+const setUp = (initialState) => {
+  const wrapper = shallow(<App />)
+  return wrapper
+}
 
 it('renders without crashing', () => {
-  // setUp()
+  setUp()
 })
