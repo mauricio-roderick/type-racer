9bcdf15d87e3c7b9361c4ea8e6e1d2bf6eefa041
Erick <roderick.mauricio@headhuntr.io>
2021-04-03 08:02:05 +0800

    Enhanced Race container, History and UserProfile component Created TypeRace component

--- src/components/Race/History/History.js
+++ src/components/Race/History/History.js
@@ -1,103 +1,65 @@
-import React, { PureComponent } from 'react'
-import { Table, Typography, message } from 'antd'
+import React, { memo } from 'react'
+import { Table, Typography } from 'antd'
 import { withRouter } from 'react-router-dom'
 import { dateTimeFormat } from '@config/collection'
-import querySting from 'query-string'
 import moment from 'moment'
+import _flow from 'lodash.flow'
 
-import { LOADING, IDLE } from '@config/constant'
-import resource from '@config/resource'
-import platormApiSvc from '@services/platform-api/'
+import { LOADING } from '@config/constant'
 
-export class RaceHistory extends PureComponent {
-  state = {
-    raceHistory: []
+const History = (props) => {
+  const { fetchStatus, records, totalRecords, queryParams: { page } } = props
+
+  const pageChangeHanlder = (page) => {
+    props.history.replace('?page=' + page)
   }
 
-  componentDidMount () {
-    this.getRaceHistory()
+  const columns = [{
+    title: 'Text Length',
+    dataIndex: 'textLength',
+    align: 'center',
+    width: '15%'
+  }, {
+    title: 'Total Time(seconds)',
+    dataIndex: 'time',
+    align: 'center',
+    width: '15%'
+  }, {
+    title: 'Speed(wpm)',
+    dataIndex: 'wpm',
+    align: 'center',
+    width: '20%'
+  }, {
+    title: 'Date & Time',
+    dataIndex: 'timestamp',
+    render: timestamp => moment(timestamp).format(dateTimeFormat.client),
+    width: '30%'
+  }]
+
+  const pagination = {
+    hideOnSinglePage: true,
+    total: totalRecords,
+    position: ['bottomCenter'],
+    pageSize: 10,
+    onChange: pageChangeHanlder,
+    current: page
   }
 
-  componentDidUpdate (prevProps) {
-    const { refresh, location: { search } } = this.props
-    if (search !== prevProps.location.search || refresh !== prevProps.refresh) {
-      this.getRaceHistory()
-    }
-  }
-
-  async getRaceHistory () {
-    let { page } = querySting.parse(this.props.location.search)
-    page = isNaN(page) ? undefined : (page - 1)
-
-    this.setState({ fetchHistoryStatus: LOADING })
-    const stateUpdate = {
-      fetchHistoryStatus: IDLE
-    }
-    try {
-      const { data } = await platormApiSvc.get(resource.raceHistory, {
-        params: { page }
-      })
-      const { totalRecords } = data.meta
-
-      stateUpdate.raceHistory = data.raceHistory
-      stateUpdate.totalRaceHistory = totalRecords
-    } catch (e) {
-      message.error('Failed to retrieve race history.')
-    }
-
-    this.setState(stateUpdate)
-  }
-
-  pageChangeHanlder = (page) => {
-    this.props.history.replace('?page=' + page)
-  }
-
-  render () {
-    const { fetchHistoryStatus, raceHistory, totalRaceHistory } = this.state
-    const { page } = querySting.parse(this.props.location.search, { parseNumbers: true })
-
-    const columns = [{
-      title: 'Text Length',
-      dataIndex: 'textLength',
-      align: 'center',
-      width: '15%'
-    }, {
-      title: 'Total Time(seconds)',
-      dataIndex: 'time',
-      align: 'center',
-      width: '15%'
-    }, {
-      title: 'Speed(wpm)',
-      dataIndex: 'wpm',
-      align: 'center',
-      width: '20%'
-    }, {
-      title: 'Date & Time',
-      dataIndex: 'timestamp',
-      render: timestamp => moment(timestamp).format(dateTimeFormat.client),
-      width: '30%'
-    }]
-
-    const pagination = {
-      hideOnSinglePage: true,
-      total: totalRaceHistory,
-      position: ['bottomCenter'],
-      pageSize: 10,
-      onChange: this.pageChangeHanlder,
-      current: page
-    }
-
-    return (
-      <Table
-        size="small"
-        columns={columns}
-        pagination={pagination}
-        loading={fetchHistoryStatus === LOADING}
-        dataSource={raceHistory.map((item, i) => ({ ...item, key: i }))}
-        title={() => <Typography.Title level={4}>Race History</Typography.Title>}
-      />
-    )
-  }
+  return (
+    <Table
+      size="small"
+      columns={columns}
+      pagination={pagination}
+      loading={fetchStatus === LOADING}
+      dataSource={records.map((item, i) => ({ ...item, key: i }))}
+      title={() => <Typography.Title level={4}>Race History</Typography.Title>}
+    />
+  )
 }
 
-export default withRouter(RaceHistory)
+const WrappedHistory = _flow([
+  withRouter,
+  memo
+])(History)
+
+export default WrappedHistory
